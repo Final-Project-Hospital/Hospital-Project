b23a2dbc1dd44d3a9a87c88933307aa4aedd2c38
Tawunchai Burakhon <161191262+Tawunchai@users.noreply.github.com>
2025-08-19 10:42:20 +0700

    add

--- frontend/src/services/index.tsx
+++ frontend/src/services/index.tsx
@@ -7,8 +7,8 @@ import { ListStatusInterface } from "../interface/IStatus";
 import { ListMiddleTargetInterface, ListRangeTargetInterface, ListTargetInterface, AddMiddleTargetInterface, AddRangeTargetInterface } from "../interface/ITarget";
 import { ListMiddleStandardInterface, ListRangeStandardInterface, ListStandardInterface, AddMiddleStandardInterface, AddRangeStandardInterface } from "../interface/IStandard";
 
-//export const apiUrl = "http://10.0.14.228:8000";
-export const apiUrl = "http://localhost:8000";
+export const apiUrl = "http://10.0.14.228:8000";
+//export const apiUrl = "http://localhost:8000";
 
 const Authorization = localStorage.getItem("token");
 
