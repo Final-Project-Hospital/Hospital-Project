60f6a9a80b62e63e5f6b1e2385eaa59b7f645ec2
Kongsavanh12 <jame.kongsawan@gmail.com>
2025-09-10 12:33:15 +0700

    update

--- backend/main.go
+++ backend/main.go
@@ -634,22 +634,9 @@ func main() {
 
 func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
-        origin := c.Request.Header.Get("Origin")
-        allowedOrigins := []string{
-            "http://localhost:5173",
-            "https://hospital-project-git-deploy-kongsavanh13s-projects.vercel.app",
-        }
-
-        for _, o := range allowedOrigins {
-            if o == origin {
-                c.Writer.Header().Set("Access-Control-Allow-Origin", o)
-                break
-            }
-        }
-
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "https://hospital-project-git-deploy-kongsavanh13s-projects.vercel.app")
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-        c.Writer.Header().Set("Access-Control-Allow-Headers",
-            "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
         c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
 
         if c.Request.Method == "OPTIONS" {
